61fbb1e810a23c8897f027bed2939fae5f80ac2d
fkamara <kofi344@gmail.com>
2018-11-30 16:00:06 +0000

    added css and images

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ from .forms import PostForm
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     posts = Post.objects.order_by('created_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
